f4280202519e1dbf78ccf47a1d99fea434674273
Rail <railbolo@gmail.com>
2022-12-02 21:48:55 +0400

    add function EnterString

--- Program.cs
+++ Program.cs
@@ -1,4 +1,48 @@
-﻿string[] array;
+﻿
+// Переменные
+string[] array;
 int count;
 string userString;
 string[] arrayTemp;
+
+// Функция ввода строк с клавиатуры и заполнение ими массива
+string[] EnterString()
+{
+  // Цикл ввода строк
+  Console.WriteLine("Вводите строки. Пустая строка означает конец ввода.");
+  count = 0;
+  array = new string[count];
+  do
+  {
+    userString = Console.ReadLine();
+    // Проверка на пусту строку
+    if (userString != "")
+    {
+      count++; // если не пустая, добавляем в массив
+      arrayTemp = new string[count];
+      for (int i = 0; i < arrayTemp.Length - 1; i++)
+      {
+        arrayTemp[i] = array[i];
+      }
+      arrayTemp[count - 1] = userString;
+      array = arrayTemp;
+    }
+  } while (userString != "");
+
+  return array;
+}
+
+EnterString();
+for (int i = 0; i < array.Length; i++)
+{
+  Console.WriteLine(i + " " + array[i]);
+}
+// Функция проверки массива по условию длины строк
+
+string[] ChangeArray(string[] array, int stringLength)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    
+  }
+}
